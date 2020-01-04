fd2b02835a365b75d97e652e70cbb4e2431f3978
mdmazlan <mdmazlan9@gmail.com>
2020-01-05 01:56:50 +0600

    first commit

---
+++ if_else_java_progam.java
@@ -0,0 +1,28 @@
+
+package com.mdmazlan.javacode.com;
+
+/**
+ *
+ * @author MAZLAN
+ */
+
+public class If_else {
+
+   public static void main(String args[]) {
+       
+      int x = 10;
+      
+      if( x == 15 ) {
+         System.out.print("Value of X is 10");
+
+      }else if( x == 20 ) {
+         System.out.print("Value of X is 20");
+
+      }else if( x == 30 ) {
+         System.out.print("Value of X is 30");
+
+      }else {
+         System.out.print("This is else statement");
+      }
+   }
+}
