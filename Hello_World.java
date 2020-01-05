1cf4cba504af611fc1f44acd0dcc4c52255de9c0
Md Mazlan <52720361+mdmazlan@users.noreply.github.com>
2020-01-06 00:45:58 +0600

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,15 @@
+
+package com.mdmazlan.javacode.com;
+
+/**
+ *
+ * @author "Md Mazlan"
+*/
+
+public class Hello_World {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World...");
+    }
+}
