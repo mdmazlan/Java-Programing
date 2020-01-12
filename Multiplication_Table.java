ed34d6a8c0bbb0dde37b6ded6ef611e63461460a
Md Mazlan <52720361+mdmazlan01@users.noreply.github.com>
2020-01-13 01:20:52 +0600

    Multiplication Table Added

---
+++ Multiplication_Table.java
@@ -0,0 +1,22 @@
+
+package com.mdmazlan.javacode.com;
+
+/**
+ *
+ * @author MAZLAN
+ */
+public class Namota {
+      
+    public static void main(String []args ){
+        
+    int no=5;
+    for (int i=1; i<=10;i=i+1)
+    {
+          System.out.printf("%d*%d=%d\n", no,i,no*i);
+          
+//         another way
+//         System.out.println(no+"*"+i+"="+(no*i));
+    }
+    
+    }
+}
