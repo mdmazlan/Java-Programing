25e4a3943767d0e003817fe41e4debd5f036e19a
Md Mazlan <52720361+mdmazlan01@users.noreply.github.com>
2020-01-15 23:55:24 +0600

    Even_Number added

---
+++ Even_Number.java
@@ -0,0 +1,21 @@
+
+package com.mdmazlan.javacode.com;
+
+/**
+ *
+ * @author MAZLAN
+ */
+
+public class even {
+    
+  public static void main(String args[])
+
+   {
+    int no=10;
+    
+    for (int i=2; i<=no; i=i+2){
+      System.out.println(i);
+
+    }
+   }
+}
