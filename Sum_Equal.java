bf1a03d087d403b56ee1959d5f4e76b30d0d7aba
Md Mazlan <52720361+mdmazlan01@users.noreply.github.com>
2020-04-10 12:05:04 +0600

    Sum_Equal added

---
+++ Sum_Equal.java
@@ -0,0 +1,20 @@
+package com.mdmazlan.javacode.com;
+
+/**
+ *
+ * @author MAZLAN
+ */
+
+public class Sum_Equal {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 20;
+        int sum=a+b;
+        
+        System.out.println("A=   "+a);
+        System.out.println("B=   "+b);
+        System.out.println("--------");
+        System.out.println("=    "+sum);
+        
+    }
+}
