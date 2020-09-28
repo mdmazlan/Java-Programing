7fd0225506977fc126e2de2cf4b57236284c4dd3
Md Mazlan <52720361+mdmazlan@users.noreply.github.com>
2020-09-28 12:06:18 -0700

    Farenhight added

---
+++ Farenhight.java
@@ -0,0 +1,17 @@
+package com.mdmazlan.javacode.com;
+
+/**
+ *
+ * @author MAZLAN
+ */
+
+public class Farenhight
+{
+	public static void main(String[] args){
+		
+    float f=98.7f;
+    float rst =5.0f /9*(f-32);
+
+	System.out.printf("%.2f \n", rst);
+	} 
+}
