617ccd300920f2eade3889198fdcc1d538fac89f
Md Mazlan <52720361+mdmazlan@users.noreply.github.com>
2021-04-24 02:55:31 +0600

    Add files via upload

---
+++ Data_Type.java
@@ -0,0 +1,33 @@
+package data_type;
+
+/**
+ *
+ * @author MAZLAN
+ */
+public class Data_Type {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   {
+        int Num =1234567891;               
+        float FloatNum =123456.99f;
+        char char_v= 'M';
+        boolean Bool = true;
+        String Txt = "MAZLAN";
+        long LNUM = 123456789L;
+        byte Byt =127;
+        double Double = 123456.355453532;
+        short  Short=12345;
+
+        System.out.println(Num);
+        System.out.println(FloatNum);
+        System.out.println(char_v);
+        System.out.println(Txt);
+        System.out.println(LNUM);
+        System.out.println(Byt);
+        System.out.print(Double);
+        System.out.println(Short);
+    }
+    }
+}
